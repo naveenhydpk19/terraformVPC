a943ee6b78da4202599e7a06092a28d0b10a4fb1
naveenhydpk19 <75367591+naveenhydpk19@users.noreply.github.com>
2021-11-06 20:14:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,13 @@ variable "environment" {
   type        = string
   description = "Name of environment this VPC is targeting"
 }
+variable "cidr_block" {
+  default     = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
+}
+variable "tags" {
+  default     = {}
+  type        = map(string)
+  description = "Extra tags to attach to the VPC resources"
+}
