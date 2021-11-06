daefbb97fea82263ad9a2372b3bb25d0d9560b9e
naveenhydpk19 <75367591+naveenhydpk19@users.noreply.github.com>
2021-11-06 20:12:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,12 @@ variable "name" {
   type        = string
   description = "Name of the VPC"
 }
+variable "project" {
+  type        = string
+  description = "Name of project this VPC is meant to house"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of environment this VPC is targeting"
+}
