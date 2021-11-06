8e1e51b10f01b6c84fde2bfeb97e08462c7b44ad
naveenhydpk19 <75367591+naveenhydpk19@users.noreply.github.com>
2021-11-06 20:54:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_vpc" "default" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
-  region = "us-east-2"
 
   tags = merge(
     {
